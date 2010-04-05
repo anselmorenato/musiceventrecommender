7103c2d7459fe771917982b6883cc42ff5405f13
derek.ingrouville <derek.ingrouville@fe7891da-235e-11df-9d62-51cc173c1019>
2010-04-05 17:40:57 +0000

    Removed class files

--- trunk/EventRecommender/src/scanners/ITunesScanner.java
+++ trunk/EventRecommender/src/scanners/ITunesScanner.java
@@ -1,38 +1,29 @@
 package scanners;
 
-import java.io.File;
+import java.util.List;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 
-import org.w3c.dom.Document;
-
-public class ITunesScanner {
-	public ITunesScanner() {
-		String file = "/Users/derek/Music/iTunes/iTunes Music Library.xml";
-		try {
-			Document d = getDocument(file);
-			
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+public class ITunesScanner implements Scanner{
+	private List<ScannerObserver> observers;
+	
+	public ITunesScanner(String itunesXML) {
+		
+		
 	}
 	
-	public Document 
-	getDocument(String file) throws Exception {
-
-		// Step 1: create a DocumentBuilderFactory
-		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-
-		// Step 2: create a DocumentBuilder
-		DocumentBuilder db = dbf.newDocumentBuilder();
-
-		Document doc = db.parse(new File(file));
-		return doc;
-	}      
+	public void scan() {
+		// TODO Auto-generated method stub
+		
+	}
 	
-	public static void main(String[] args) {
-		ITunesScanner isc = new ITunesScanner();
+	private void notifyObservers(SongScanner s) {
+		for (ScannerObserver o : this.observers) {
+			o.songFound(s.getTitle(), s.getArtist(), 0);
+		}
+	}
+
+	public void addObserver(ScannerObserver o) {
+		this.observers.add(o);
+		
 	}
 }
