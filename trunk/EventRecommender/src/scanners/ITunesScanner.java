6eec8f88db49fb73b744c2708c8ca3c0b2ac3c5b
derek.ingrouville <derek.ingrouville@fe7891da-235e-11df-9d62-51cc173c1019>
2010-04-05 23:04:01 +0000

    iTunes Scanner implemented

--- trunk/EventRecommender/src/scanners/ITunesScanner.java
+++ trunk/EventRecommender/src/scanners/ITunesScanner.java
@@ -1,29 +1,64 @@
 package scanners;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
 import java.util.List;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
-public class ITunesScanner implements Scanner{
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+
+public class ITunesScanner implements Scanner {
 	private List<ScannerObserver> observers;
+	String xmlFile;
+	DefaultHandler handler;
+	SAXParser saxParser;
+	int count = 0;
 	
 	public ITunesScanner(String itunesXML) {
-		
+		xmlFile = itunesXML;
+		handler = new ITunesXMLHandler(this);
+		this.observers = new LinkedList<ScannerObserver>();
+	
+        // Use the default (non-validating) parser
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        try {
+			saxParser = factory.newSAXParser();
+		} catch (ParserConfigurationException e) {
+		} catch (SAXException e) {
+		}
 		
 	}
 	
 	public void scan() {
-		// TODO Auto-generated method stub
+		 try {
+			saxParser.parse(new File(xmlFile), handler);
+		} catch (SAXException e) {
+		} catch (IOException e) {
+		}
 		
 	}
 	
-	private void notifyObservers(SongScanner s) {
+	public void notifyObservers(String title, String artist, int playcount) {
 		for (ScannerObserver o : this.observers) {
-			o.songFound(s.getTitle(), s.getArtist(), 0);
+			o.songFound(title, artist, playcount);
 		}
 	}
 
 	public void addObserver(ScannerObserver o) {
 		this.observers.add(o);
-		
+	}
+	
+	public static void main(String[] args) {
+		String file = "/Users/derek/Music/iTunes/iTunes Music Library.xml";
+				
+		ITunesScanner scanner = new ITunesScanner(file);
+		scanner.scan();
 	}
 }
